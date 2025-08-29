b3d12485bdac9f41c3e1ce4f929c7d16ec2e7f4b
Rui Freitas <rodloboz@hey.com>
2025-08-29 20:38:32 +0100

    Fixed incomplete @type opts() specification

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -2,7 +2,7 @@
   # The spec is intentionally more general than the implementation
   # to allow for future versions without breaking the API
   {"lib/lotus/migrations.ex", :contract_supertype, 17},
-  
+
   # opts() type is intentionally flexible for future expansion
   {"lib/lotus.ex", :contract_supertype, 101}
 ]
